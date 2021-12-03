9b7bde11b47ffa1311a1da806c2a14b0f9eab7cf
Dinesh <dinu1763@gmail.com>
2021-12-03 22:20:30 +0530

    express-mvc

--- Express MVC/src/app.js
+++ Express MVC/src/app.js
@@ -6,6 +6,18 @@ app.use(express.json());
 
 const connect = require('./configs/db');
 
+
+const userController = require('./controller/user.controller');
+const topicController = require('./controller/topic.controller');
+const studentController = require('./controller/student.controller');
+const evaluationController = require('./controller/evaluation.controller');
+
+app.use('/user', userController);
+app.use('/topic', topicController);
+app.use('/student', studentController);
+app.use('/evaluation', evaluationController);
+
+
 app.listen(8000, async () =>{
     await connect();
     console.log('listening on 8000 port');
